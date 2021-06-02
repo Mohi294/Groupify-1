0cd0521bb4a8fa883704efb463d157a3d0486f74
mohi294 <mahmadkhani294@gmail.com>
2021-06-02 23:40:49 +0430

    still widget_twearks problem

--- studymate/settings.py
+++ studymate/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'widget_tweaks',
+    # 'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
