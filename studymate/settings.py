d9dfaec6413b5dd9c4b65884118c264c71f32f20
mohi294 <mahmadkhani294@gmail.com>
2021-06-11 00:04:06 +0430

    ready to test

--- studymate/settings.py
+++ studymate/settings.py
@@ -28,7 +28,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1','locahost']
 # CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS=['http://127.0.0.1:3000,http://localhost:3000']
+CORS_ALLOWED_ORIGINS=['http://127.0.0.1:3000','http://localhost:3000']
 
 # Application definition
 
