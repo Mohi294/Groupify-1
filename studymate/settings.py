ac225615ffea1888fdeac3a9a7484ce683cee7e7
mohi294 <mahmadkhani294@gmail.com>
2021-06-10 21:45:52 +0430

    ready to test

--- studymate/settings.py
+++ studymate/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-!!hdf_&6@kfk(6x_so8s*ca1v7eer736n^zbhesgfz2l30r8ro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1, localhost']
+
+CORS_ALLOWED_ORIGINS=['http://127.0.0.1:3000,http://localhost:3000']
 
 # Application definition
 
