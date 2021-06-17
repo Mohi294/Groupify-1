405a321a0f0d8d063073f5001ec06bb92a952d65
mohi294 <mahmadkhani294@gmail.com>
2021-06-18 03:18:37 +0430

    some dubugging stuffs

--- collaborate/views.py
+++ collaborate/views.py
@@ -148,7 +148,6 @@ class AnswerJoinRequest(UpdateAPIView):
             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
         else:
             instance.delete()
-            self.perform_destroy(serializer)
             return Response(serializer.data, status=status.HTTP_200_OK)
 
 
